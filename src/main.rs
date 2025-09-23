2b43c3c53078529b979719736b932040f05ec770
Ivor <dly20011214@outlook.com>
2025-09-24 00:19:48 +0800

    修改日志框架到tklog，但现阶段tklog似乎还有很多问题，暂时观察

--- src/main.rs
+++ src/main.rs
@@ -1,77 +1,108 @@
-use reqwest::{self, Client, ClientBuilder};
+//异步运行时
 use futures::future::join_all;
 use tokio;
-use std::sync::LazyLock;
-use std::net::{IpAddr, Ipv4Addr, Ipv6Addr};
-use std::str::FromStr;
+use tokio::time::sleep;
+//标准库
+#[cfg(debug_assertions)]
+use std::env::current_dir;
+#[cfg(not(debug_assertions))]
 use std::env::current_exe;
 use std::fs::read_to_string;
+use std::net::{IpAddr, Ipv4Addr, Ipv6Addr};
+use std::str::FromStr;
+use std::sync::LazyLock;
 use std::time::Duration;
+//日志
+#[cfg(not(debug_assertions))]
+use tklog::MODE;
+use tklog::{
+    ASYNC_LOG, Format, LEVEL, LOG, async_debug, async_error, async_fatal, async_info, info,
+};
+//io
 use json::{self, JsonValue};
-use flexi_logger::{Logger, Criterion, Naming, Cleanup, Age, Duplicate, FileSpec, WriteMode, colored_detailed_format,detailed_format};
-use log::{debug, error, info, warn, trace};
+use reqwest::{self, Client, ClientBuilder, retry};
 
-// schtasks /create /tn test /sc MINUTE /mo 2 /tr a:\test.bat /ru System
-
-static CLIENT: LazyLock<Client> = LazyLock::new(||{
+static CLIENT: LazyLock<Client> = LazyLock::new(|| {
     ClientBuilder::new()
-    .no_proxy()
-    .https_only(true)
-    .gzip(true)
-    .connect_timeout(Duration::new(5, 0))
-    .read_timeout(Duration::new(5, 0))
-    .build()
-    .unwrap()
+        .no_proxy()
+        .https_only(true)
+        .gzip(true)
+        .connect_timeout(Duration::new(5, 0))
+        .read_timeout(Duration::new(5, 0))
+        .retry(retry::for_host("*").max_retries_per_request(3))
+        .build()
+        .unwrap()
 });
 
-async fn ask_api(ip: IpAddr, info: &mut JsonValue) -> Result<(),()> {
-    let api_url = format!("https://api.cloudflare.com/client/v4/zones/{}/dns_records/{}",info.remove("zone_id"), info.remove("dns_id"));
+async fn ask_api(ip: IpAddr, info: &mut JsonValue) -> Result<(), ()> {
+    let api_url = format!(
+        "https://api.cloudflare.com/client/v4/zones/{}/dns_records/{}",
+        info.remove("zone_id"),
+        info.remove("dns_id")
+    );
     let header = format!("Bearer {}", info.remove("api_token"));
     info["content"] = ip.to_string().into();
-    match CLIENT.put(api_url)
+    match CLIENT
+        .put(api_url)
         .body(info.dump())
         .header("Content-Type", "application/json")
         .header("Authorization", header)
-        .send().await{
-            Ok(success) => {
-                if success.status().is_success(){
-                    debug!("更新: {}, 类型: {}成功", info["name"], info["type"]);
-                    // success
-                }else{
-                    warn!("更新:{},类型:{}时服务器返回码:{}", info["name"], info["type"], success.status().as_u16());
-                    return Err(());
-                }
-            },
-            Err(error) =>{
-                debug!("{error}");
-                if error.is_timeout(){
-                    warn!("更新:{},类型:{}时链接超时", info["name"], info["type"]);
-                }else if error.is_connect() {
-                    warn!("更新:{},类型:{}时链接错误", info["name"], info["type"]);
-                }else {
-                    warn!("更新{}类型{}时发生未知错误:{}", info["name"], info["type"], error);
-                }
+        .send()
+        .await
+    {
+        Ok(success) => {
+            if success.status().is_success() {
+                async_debug!("更新: ", info["name"], "类型: ", info["type"], "成功");
+                // success
+            } else {
+                async_error!(
+                    "更新:",
+                    info["name"],
+                    ",类型:",
+                    info["type"],
+                    "时服务器返回码:",
+                    success.status().as_u16()
+                );
                 return Err(());
             }
-        };
+        }
+        Err(error) => {
+            async_debug!("{error}");
+            if error.is_timeout() {
+                async_error!("更新: ", info["name"], "类型: ", info["type"], "时链接超时");
+            } else if error.is_connect() {
+                async_error!("更新: ", info["name"], "类型: ", info["type"], "时链接错误");
+            } else {
+                async_error!(
+                    "更新: ",
+                    info["name"],
+                    "类型: ",
+                    info["type"],
+                    "时发生其他错误: ",
+                    error
+                );
+            }
+            return Err(());
+        }
+    };
     Ok(())
 }
 
-async fn update_ip(ip_version: u8, global_config_json: &JsonValue){
+async fn update_ip(ip_version: u8, global_config_json: &JsonValue) {
     let ip = match get_ip(ip_version).await {
         Ok(success) => {
-            debug!("获取IPv{ip_version}成功");
+            async_debug!("获取IPv", ip_version, "成功");
             success
-        },
-        Err(_) => return
+        }
+        Err(_) => return,
     };
 
     let mut self_config_json = global_config_json.clone();
     let mut ask_api_list = Vec::new();
-    for i in self_config_json.members_mut(){
-        if i["type"]=="A" && ip_version==4{
+    for i in self_config_json.members_mut() {
+        if i["type"] == "A" && ip_version == 4 {
             ask_api_list.push(ask_api(ip, i));
-        }else if i["type"]=="AAAA" &&ip_version==6{
+        } else if i["type"] == "AAAA" && ip_version == 6 {
             ask_api_list.push(ask_api(ip, i));
         }
     }
@@ -79,155 +110,155 @@ async fn update_ip(ip_version: u8, global_config_json: &JsonValue){
 }
 
 async fn get_ip(ip_version: u8) -> Result<IpAddr, ()> {
-    // let url = format!("https://{ip_version}.ipw.cn/");
-    let url = format!("https://ipv{ip_version}.icanhazip.com/");
-    let ip_response = match CLIENT.get(url).send().await{
+    let ip_response = match CLIENT
+        .get(format!("https://ipv{ip_version}.icanhazip.com/"))
+        .send()
+        .await
+    {
         Ok(success) => {
-            if success.status().is_success(){
+            if success.status().is_success() {
                 success
-            }else {
-                warn!("获取ipv{ip_version}时状态码不正确{}",success.status().as_u16());
+            } else {
+                async_error!(
+                    "获取ipv",
+                    ip_version,
+                    "时状态码不正确",
+                    success.status().as_u16()
+                );
                 return Err(());
             }
-        },
+        }
         Err(error) => {
-            if error.is_timeout(){
-                warn!("获取ipv{ip_version}时链接超时")
-            }else if error.is_connect() {
-                warn!("获取ipv{ip_version}时链接错误")
-            }else {
-                warn!("获取ipv{ip_version}时发生未定义错误{}",error)
+            if error.is_timeout() {
+                async_error!("获取ipv", ip_version, "时链接超时")
+            } else if error.is_connect() {
+                async_error!("获取ipv", ip_version, "时链接错误")
+            } else {
+                async_error!("获取ipv", ip_version, "时发生其他错误: ", error)
             }
             return Err(());
         }
     };
-    
-    let ip_text = match ip_response.text().await{
+
+    let ip_text = match ip_response.text().await {
         Ok(success) => success,
         Err(error) => {
-            warn!("获取IPv{ip_version}时响应正文时发生错误：{error}");
+            async_error!("获取IPv", ip_version, "时响应正文时发生错误: ", error);
             return Err(());
         }
     };
 
     match ip_version {
-        4 => {
-            match Ipv4Addr::from_str(ip_text.trim()) {
-                Ok(ip) => return Ok(IpAddr::V4(ip)),
-                Err(_) => {
-                    warn!("获取到格式不正确的ipv4");
-                    return Err(());
-                }
+        4 => match Ipv4Addr::from_str(ip_text.trim()) {
+            Ok(ip) => return Ok(IpAddr::V4(ip)),
+            Err(_) => {
+                async_error!("获取到格式不正确的ipv4");
+                return Err(());
             }
         },
-        6 => {
-            match Ipv6Addr::from_str(ip_text.trim()) {
-                Ok(ip) => return Ok(IpAddr::V6(ip)),
-                Err(_) => {
-                    warn!("获取到格式不正确的ipv6");
-                    return Err(());
-                }
+        6 => match Ipv6Addr::from_str(ip_text.trim()) {
+            Ok(ip) => return Ok(IpAddr::V6(ip)),
+            Err(_) => {
+                async_error!("获取到格式不正确的ipv6");
+                return Err(());
             }
         },
         _ => {
-            error!("程序内部错误：get_ip函数获取到不可能的值{ip_version}");
-            panic!();
+            async_fatal!("程序内部错误: get_ip函数获取到不可能的值: ", ip_version);
+            panic!("程序内部错误: get_ip函数获取到不可能的值{ip_version}");
         }
     }
 }
 
-fn main() {
-    let mut config_json_path = match current_exe() {
-        Ok(success) => success,
-        Err(_) => {
-            error!("文件系统错误！无法读取config.json");
-            return;
-        }
+#[tokio::main]
+async fn main() {
+    /*当前二进制文件路径*/
+    #[cfg(not(debug_assertions))]
+    let exe_dir = {
+        let mut path = current_exe().expect("无法读取二进制文件位置");
+        path.pop();
+        path
     };
-    config_json_path.pop();
-    let mut log_path = config_json_path.clone();
-    config_json_path.push("config.json");
-    log_path.push("logs");
+    /*初始化日志*/
+    ASYNC_LOG
+        .set_console(true)
+        .set_format(Format::LevelFlag | Format::Time | Format::ShortFileName);
+    // LOG.set_console(true)
+    //     .set_format(Format::LevelFlag | Format::Time | Format::ShortFileName);
 
-    let _logger = Logger::try_with_str("info")
-        .unwrap()
-        .log_to_file(FileSpec::default()
-            .directory(log_path) //定义日志文件位置
-            .basename("ddns")) //定义日志文件名，不包含后缀
-        .duplicate_to_stdout(Duplicate::Trace) //复制日志到控制台
-        .rotate(
-            Criterion::Age(Age::Day), // 按天轮转
-            Naming::TimestampsCustomFormat {
-                current_infix: None,
-                format: "%Y-%m-%d"
-            },       // 文件名包含日期并以天为单位轮换
-            Cleanup::KeepCompressedFiles(15), // 保留15天日志并启用压缩
+    #[cfg(not(debug_assertions))]
+    ASYNC_LOG
+        .set_level(LEVEL::Info)
+        .set_cutmode_by_time(
+            exe_dir.join("logs/ddns.log").to_str().unwrap(),
+            MODE::DAY,
+            15,
+            true,
         )
-        .format_for_stdout(colored_detailed_format) //控制台输出彩色带时间的日志格式
-        .format_for_files(detailed_format) //文件中使用ANSI颜色会乱码，所以使用无颜色格式
-        .write_mode(WriteMode::Async)
-        .append() //指定日志文件为添加内容而不是覆盖重写
-        .start()
-        .unwrap();
+        .await;
+    // #[cfg(not(debug_assertions))]
+    // LOG.set_level(LEVEL::Info).set_cutmode_by_time(
+    //     exe_dir.join("logs/ddns.log").to_str().unwrap(),
+    //     MODE::DAY,
+    //     15,
+    //     true,
+    // );
 
+    #[cfg(debug_assertions)]
+    ASYNC_LOG.set_level(LEVEL::Debug);
+    // LOG.set_level(LEVEL::Debug);
 
+    #[cfg(not(debug_assertions))]
+    let config_path = exe_dir.join("config.json");
+    #[cfg(debug_assertions)]
+    let config_path = current_dir().unwrap().join("config.json");
 
-    let config_json_text = match read_to_string(config_json_path){
+    let config_json_text = match read_to_string(config_path) {
         Ok(success) => {
-            trace!("成功读取配置文件");
+            async_debug!("成功读取配置文件");
             success
-        },
+        }
         Err(_) => {
-            error!("读取失败,请检查config.json是否存在并使用UTF-8编码");
-            return;
+            async_fatal!("读取失败,请检查config.json是否存在并使用UTF-8编码");
+            panic!("读取失败,请检查config.json是否存在并使用UTF-8编码");
         }
     };
-    let config_json = match json::parse(config_json_text.as_str()){
+    let config_json = match json::parse(config_json_text.as_str()) {
         Ok(success) => {
-            trace!("成功解析json");
+            async_debug!("成功解析json");
             success
-        },
+        }
         Err(_) => {
-            error!("json文件格式不正确");
-            return;
+            async_fatal!("json文件格式不正确");
+            panic!("json文件格式不正确")
         }
     };
 
-    // println!("{json_data}");
     let mut have_ipv4 = false;
     let mut have_ipv6 = false;
 
-    for i in config_json.members(){
-        if i["type"] == "A"{
-            debug!("找到A记录需要解析");
+    for i in config_json.members() {
+        if i["type"] == "A" {
+            async_debug!("找到A记录需要解析");
             have_ipv4 = true;
-        }else if i["type"] == "AAAA"{
-            debug!("找到AAAA记录需要解析");
+        } else if i["type"] == "AAAA" {
+            async_debug!("找到AAAA记录需要解析");
             have_ipv6 = true;
         }
-        if have_ipv4 && have_ipv6{
+        if have_ipv4 && have_ipv6 {
             break;
         }
     }
 
     let mut update_tasks = Vec::new();
-    if have_ipv4{
+    if have_ipv4 {
         update_tasks.push(update_ip(4, &config_json));
     }
-    if have_ipv6{
+    if have_ipv6 {
         update_tasks.push(update_ip(6, &config_json));
-    } 
-    
-    let rt = match tokio::runtime::Runtime::new() {
-        Ok(success) => {
-            trace!("成功创建异步运行时");
-            success
-        },
-        Err(error) => {
-            error!("无法创建异步运行时,回溯错误:{error}");
-            return;
-        }
-    };
-    rt.block_on(join_all(update_tasks));
-    info!("done");
+    }
+
+    join_all(update_tasks).await;
+    async_info!("本次更新任务完成");
+    sleep(Duration::from_millis(500)).await;
 }
